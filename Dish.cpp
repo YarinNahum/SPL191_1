efadafb2abc3a7bb0c241e80287fb39d7ed08318
ravidlevy99 <ravidle@post.bgu.ac.il>
2018-11-07 21:09:07 +0200

    Dish

---
+++ Dish.cpp
@@ -0,0 +1,40 @@
+//
+// Created by ravidlevy99 on 11/7/18.
+#include <string>
+//
+
+#ifndef SPL191_1_DISH_H
+#define SPL191_1_DISH_H
+
+enum DishType{
+
+};
+
+class Dish{
+public:
+    Dish(int d_id, std::string d_name, int d_price, DishType d_type): id(d_id), name(d_name), price(d_price), type(d_type){}
+    int getId()
+    {
+            return id;
+    }
+    std::string getName()
+    {
+            return name;
+    }
+    int getPrice()
+    {
+            return price;
+    }
+    DishType getType()
+    {
+            return type;
+    }
+private:
+    const int id;
+    const std::string name;
+    const int price;
+    const DishType type;
+};
+
+
+#endif //SPL191_1_DISH_H
