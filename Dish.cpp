21dba807e7f814a5f03401bf91731d8ce4c8587c
ravidlevy99 <ravidle@post.bgu.ac.il>
2018-11-08 18:29:54 +0200

    8.11

--- Dish.cpp
+++ Dish.cpp
@@ -1,40 +1,26 @@
 //
 // Created by ravidlevy99 on 11/7/18.
-#include <string>
+
 //
+#include <string>
+#include "Dish.h"
 
-#ifndef SPL191_1_DISH_H
-#define SPL191_1_DISH_H
-
-enum DishType{
-    VEG, SPC, BVG, ALC
-};
-
-class Dish{
-public:
-    Dish(int d_id, std::string d_name, int d_price, DishType d_type): id(d_id), name(d_name), price(d_price), type(d_type){}
-    int getId()
-    {
-            return id;
-    }
-    std::string getName()
-    {
-            return name;
-    }
-    int getPrice()
-    {
-            return price;
-    }
-    DishType getType()
-    {
-            return type;
-    }
-private:
-    const int id;
-    const std::string name;
-    const int price;
-    const DishType type;
-};
-
-
-#endif //SPL191_1_DISH_H
+Dish::Dish(int d_id, std::string d_name, int d_price, DishType d_type): id(d_id), name(d_name), price(d_price), type(d_type){}
+Dish::Dish(const Dish &other): id(other.id), name(other.name), price(other.price), type(other.type){}
+int Dish::getId() const
+{
+    return id;
+}
+std::string Dish::getName() const
+{
+    return name;
+}
+int Dish::getPrice() const
+{
+    return price;
+}
+DishType Dish::getType() const
+{
+    return type;
+}
+//SPL191_1_DISH_H
