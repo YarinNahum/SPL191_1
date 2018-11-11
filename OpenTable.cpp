e40cb559b01f5487d2114cf5b48d2f6da3623684
YarinNahum <yarinna@post.bgu.ac.il>
2018-11-12 00:25:45 +0200

    11.11.1.11

--- OpenTable.cpp
+++ OpenTable.cpp
@@ -1,4 +1,32 @@
 
-#include "OpenTable.h"
 #include "Customer.h"
 #include "Table.h"
+#include "Action.h"
+
+
+OpenTable::OpenTable(int id, std::vector<Customer *> &customersList): tableId(id) , customers(customersList) {}
+void OpenTable::act(Restaurant &restaurant) {
+    Table* t = restaurant.getTable();
+    if(t == nullptr || t->isOpen())
+        std::cout << "Table does not exits or is already open";
+    else {
+        t->setNumOfCustomers(customers.size());
+        t->setCustomerList(customers);
+        t->openTable();
+        std::cout << toString();
+    }
+
+}
+
+std::string OpenTable::toString() const {
+    std::string output = "open " + tableId;
+    output += " ";
+    for(auto i : customers)
+    {
+        output += i->toString();
+        output += " ";
+    }
+    output += "\n";
+    return output;
+}
+
